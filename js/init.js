a8c601a5be3a3e7033d6e190c570e4da89c4b8c3
andrewbingham1973 <andrewbingham@gmail.com>
2017-01-31 14:25:30 +0000

    update to init.js
    slight modification to reduce code for partial view generation in later
    code.

---
+++ js/init.js
@@ -0,0 +1,34 @@
+//initialise register screen on first run
+getScreenContent('_register');
+
+$(".navBack").css("visibility", "hidden");
+
+
+
+(function($){
+  $(function(){
+
+
+
+        // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+        //$('.modal').modal();
+
+
+ //$('.modal-trigger').leanModal();
+//initialise main menu on start
+ //$('.modal').modal();
+$('select').material_select();
+$('.button-collapse').sideNav();
+    //$('.button-collapse').collapsible();
+    //$('.button-collapse').sideNav('show');
+
+    var $container = $('#masonry-grid');
+    // initialize
+    $container.masonry({
+      columnWidth: '.col',
+      itemSelector: '.col',
+    });
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
